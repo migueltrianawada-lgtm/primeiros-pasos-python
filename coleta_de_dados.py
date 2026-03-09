10b61468b9b7fcd0ad8e8235f42144272471a416
Miguel <104658338+miguelpalacios1987@users.noreply.github.com>
2026-03-09 10:21:14 -0300

    coletando dados com o metodo o input

---
+++ coleta_de_dados.py
@@ -0,0 +1,7 @@
+nome  = input('escreva seu nome:  ')
+print(nome)
+ano_entrada = int(input('Escreva o ano de ingresso do(a) estudante: '))
+print(ano_entrada,type(ano_entrada))
+
+nota_entrada = float(input('Digite a nota do teste de ingresso: '))
+print(f'Ano de entrada {ano_entrada} - nota do teste de ingreso {nota_entrada}')
---
+++ variaveis_string.py
@@ -0,0 +1,15 @@
+s1 = 'Alura'
+s2 = "Alura"
+print(type(s1),type(s2))
+
+texto = '  Geovana Alessandra dias Sanyos '
+texto.upper()
+print(texto.upper())
+texto.lower()
+print(texto.lower())
+texto.strip
+print(texto.strip())
+texto.replace
+print(texto.replace('y','t'))
+texto = texto.strip().replace('y','t').upper()
+print(texto)
