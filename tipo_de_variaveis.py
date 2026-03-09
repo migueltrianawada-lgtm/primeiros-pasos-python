fa8cd984396c3983df9474dabae1b344b6dad4ff
Miguel <104658338+miguelpalacios1987@users.noreply.github.com>
2026-03-09 08:15:24 -0300

    variaveis

---
+++ tipo_de_variaveis.py
@@ -0,0 +1,14 @@
+i = 5
+type(i)
+f = 7.5
+type(f)
+s = 'Mirla'
+type(s)
+b = True
+type(b)
+print(i,f,s,b)
+
+nome = 'Fabricio Daniel'
+idade = 15
+media_do_semestre = 8,45
+situacao_de_aprovacao = True
---
+++ variaveis.py
@@ -0,0 +1,6 @@
+idade = 5
+print(idade)
+idade = 10
+print(idade)
+nome = 'gabriel'
+print(nome)
