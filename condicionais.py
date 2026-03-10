1c019dfc9f176a51badb79e8ab506f6fc0f4b7c2
Miguel <104658338+miguelpalacios1987@users.noreply.github.com>
2026-03-10 08:35:59 -0300

    condicionais if,elif,else

---
+++ condicionais.py
@@ -0,0 +1,31 @@
+media = float(input('Digite a média: '))
+if media>= 6.0:
+  print('aprovado(a)')
+else:
+  print('reprovado(a)')
+
+
+  media = float(input('Digite a média: '))
+if media >= 6.0:
+  print('Aprovado(a)')
+if 6.0 > media >= 4.0:
+  print('Recuperação')
+if media < 4.0:
+  print('Reprovado')
+
+  media = float(input('Digite a média: '))
+if media >= 6.0:
+  print('Aprovado(a)')
+if 6.0 > media >= 4.0:
+  print('Recuperação')
+if media < 4.0:
+  print('Reprovado')
+
+
+  media = float(input('Digite a média: '))
+if media >= 6.0:
+  print('Aprovado(a)')
+elif 6.0 > media >= 4.0:
+  print('Recuperação')
+else:
+  print('Reprovado')
