90cafb3eed9853331b77d3242ee4fab24d059518
Miguel <104658338+miguelpalacios1987@users.noreply.github.com>
2026-03-11 10:22:35 -0300

    listas

---
+++ dicionario.py
@@ -0,0 +1,9 @@
+dicionario = {'chave_1':1,
+              'chave_2':2}
+print(dicionario)
+
+estudante = {'matricula':2000168933,
+             'dia de cadastro':25,
+             'mes de cadastro':10,
+             'turma':'2E',}
+print(estudante)
--- listas.py
+++ listas.py
@@ -1,6 +1,6 @@
 lista = ['Fabricio Daniel', 9.5 , 9.0 , 8.0 , True]
 lista[0],lista[1]
-
+#metodo join
 misturas = ['Tintas: vermelho, azul e amarelo',
             'Verde: mistura de azul e amarelo',
             'Laranja: mistura de vermelho e amarelo',
@@ -8,7 +8,27 @@ misturas = ['Tintas: vermelho, azul e amarelo',
 unificador = '. '
 string_misturas = unificador.join(misturas)
 print(string_misturas)
-
+#metodo slipt
 duvida = 'Quem veio antes? O ovo? Ou foi a serpente?'
 lista_palavras = duvida.split('?')
 print(lista_palavras)
+media = (lista[1] + lista[2] + lista[3])/3
+media
+#metodo append
+lista.append(media)
+#metodo insert
+raca_caes = ['Labrador Retriever',
+             'Bulldog Francês',
+             'Pastor Alemão',
+             'Poodle']
+raca_caes.insert(1, 'Golden Retriever')
+print (raca_caes)
+
+#metodo .pop
+
+raca_caes.pop(1)
+print (raca_caes)
+#metodo .sort
+raca_caes.sort()
+print (raca_caes)
+
